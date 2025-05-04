22bfa4ccb5f339538842ce9a8fb95bb4d840d40c
Alif Abdullah <alifabdullah7040@gmail.com>
2025-05-04 12:32:42 -0400

    Vercel production agogo

---
+++ README.md
@@ -0,0 +1,1 @@
+This repository houses the backend code of my Cleanly project.
--- api/index.js
+++ api/index.js
@@ -3,6 +3,8 @@ const app = express();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
+app.get("/intake", (req, res) => res.send("How did you get here?"));
+
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
