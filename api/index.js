79d78ba33930ddef86bbf8d8f40d03adb9143bbb
Alif Abdullah <alifabdullah7040@gmail.com>
2025-05-05 12:40:59 -0400

    Began work to see what is causing the backend to break apart

--- api/index.js
+++ api/index.js
@@ -1,10 +1,35 @@
 const express = require("express");
 const app = express();
+const cors = require('cors')
+const mongoose = require('mongoose')
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+
+
+require('dotenv').config()
+const { DB_URL, SERVER_APP, PORT } = process.env
+console.log(process.env)
+
+
+const corsOptions = {
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+
+app.use(cors(corsOptions));
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+  console.log("SERVER hit the home page");
+  res.send("CLIENT hit the home page");
+})
+
+
 
 app.get("/intake", (req, res) => res.send("How did you get here?"));
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+app.listen(8080, () => console.log("Server ready on port 8080."));
 
 module.exports = app;
